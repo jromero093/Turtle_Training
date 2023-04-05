4d482e90e8a78c009b298f724c10dc5d70865bc5
jromero093 <127344540+jromero093@users.noreply.github.com>
2023-04-05 16:14:21 -0400

    Update turtle_training.py
    Updated turtle training file

--- turtle_training.py
+++ turtle_training.py
@@ -5,14 +5,16 @@ s = turtle.getscreen
 turtle.title("Turtle Training Academy - You Gotta Train Your Turtle")
 turtle.bgcolor("lavender")
 
+# This variable name (t) is what we will call our turtle
 t = turtle.Turtle()
+# Appropriately shaped
 t.shape("turtle")
 
 t.pensize(5)
 t.pencolor("white")
 t.fillcolor("light blue")
 
-# I turned ceertain things into functions to call upon later
+# I turned certain things into functions to call upon later
 # as opposed to deleting the code when not in use.
 def myshape():
     t.right(90)
@@ -31,7 +33,7 @@ def rectangle():
     t.fd(200)
     t.rt(90)
     t.fd(100)
-    t.end_fill()
+    
 
 def pick_pen_up():
     t.penup()
@@ -41,10 +43,25 @@ def pick_pen_up():
     t.pendown()
     t.forward(125)
 
+def circles_w_clone():
+    t.begin_fill()
+    t.circle(90)
+    t.end_fill()
 
-t.begin_fill()
-t.circle(90)
-t.end_fill()
+    # Our second turtle
+    c = t.clone()
+    c.circle(50)
 
-c = t.clone()
-c.circle(50)
+def six_circles():
+    n = 10 
+    while n <= 60:
+        t.circle(n)
+        n = n + 10
+
+x = input("Would you like me to draw you a shape? [y]es or [n]o: ")
+if x == "y":
+    circles_w_clone()
+elif x == "n":
+    print("Well, okay then.")
+else:
+    print("Hm? Invalid reply. Terminating program.")
